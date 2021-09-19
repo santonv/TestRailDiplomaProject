bd88c195659cf10ca45604e2cfcb496294ac4e61
Anton Sidorik <71692319+santonv@users.noreply.github.com>
2021-09-19 14:17:03 +0300

    Update TestCaseTest.java

--- src/test/java/Tests/TestCaseTest.java
+++ src/test/java/Tests/TestCaseTest.java
@@ -37,30 +37,30 @@ public class TestCaseTest extends BaseTest{
                 createNewTestCase(testCase);
     }
 
-    @Test(description = "Delete Test Case")
-    public void deleteTestCase(){
-        TestCase testCase = TestCaseFactory.get();
-        BaseSteps baseSteps = new BaseSteps(driver);
-        baseSteps.openLoginPage(UserConfig.getBaseUrl()).
-                isLoginPageOpened();
-        LoginPageSteps loginPageSteps = new LoginPageSteps(driver);
-        loginPageSteps.inputEmailAndPassword(UserConfig.getEMAIL(),UserConfig.getPASSWORD()).
-                clickLoginButton().
-                isHomePageOpened();
-        ProjectSteps projectSteps = new ProjectSteps(driver);
-        projectSteps.
-                clickAddNewProjectButton().
-                isAddProjectPageOpened().
-                createNewProject("Use a single repository","ProjectForDeleteTestCase2").
-                isProjectsPageOpened().
-                getMessageAfterCreatedProject().
-                checkProjectIsExistOnTheProjectsPage("ProjectForDeleteTestCase2").
-                openProjectDetailsPage("ProjectForDeleteTestCase2");
-        TestCaseSteps testCaseSteps = new TestCaseSteps(driver);
-        testCaseSteps.
-                clickAdd().
-                isAddTestCasePadeIsOpened().
-                createNewTestCase(testCase).
-                deleteTestCase(testCase);
-    }
+//     @Test(description = "Delete Test Case")
+//     public void deleteTestCase(){
+//         TestCase testCase = TestCaseFactory.get();
+//         BaseSteps baseSteps = new BaseSteps(driver);
+//         baseSteps.openLoginPage(UserConfig.getBaseUrl()).
+//                 isLoginPageOpened();
+//         LoginPageSteps loginPageSteps = new LoginPageSteps(driver);
+//         loginPageSteps.inputEmailAndPassword(UserConfig.getEMAIL(),UserConfig.getPASSWORD()).
+//                 clickLoginButton().
+//                 isHomePageOpened();
+//         ProjectSteps projectSteps = new ProjectSteps(driver);
+//         projectSteps.
+//                 clickAddNewProjectButton().
+//                 isAddProjectPageOpened().
+//                 createNewProject("Use a single repository","ProjectForDeleteTestCase2").
+//                 isProjectsPageOpened().
+//                 getMessageAfterCreatedProject().
+//                 checkProjectIsExistOnTheProjectsPage("ProjectForDeleteTestCase2").
+//                 openProjectDetailsPage("ProjectForDeleteTestCase2");
+//         TestCaseSteps testCaseSteps = new TestCaseSteps(driver);
+//         testCaseSteps.
+//                 clickAdd().
+//                 isAddTestCasePadeIsOpened().
+//                 createNewTestCase(testCase).
+//                 deleteTestCase(testCase);
+//     }
 }
