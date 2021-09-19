8683b53cc6904bbe9666016f47e6c392f9692a75
Anton Sidorik <71692319+santonv@users.noreply.github.com>
2021-09-19 12:18:32 +0300

    Update UserConfig.java

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -2,9 +2,9 @@ package config;
 
 public class UserConfig {
 
-    private static final String EMAIL = "sidorik.a97@gmail.com";
-    private static final String PASSWORD = "UtblKrUW5UijCYr6PfQc";
-    private static final String BASE_URL = "https://sidorika97.testrail.io/";
+    private static final String EMAIL = "darkmank79@gmail.com";
+    private static final String PASSWORD = "lg8R6gl4gjml/2yvEqLr";
+    private static final String BASE_URL = "https://darkmank97.testrail.io/";
 
     public static String getEMAIL() {
         return EMAIL;
