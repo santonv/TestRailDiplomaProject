df3071929995cd3e2df83a43b8dea194e921d0ba
Anton Sidorik <71692319+santonv@users.noreply.github.com>
2021-10-06 16:33:56 +0300

    Update UserConfig.java

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -2,9 +2,9 @@ package config;
 
 public class UserConfig {
 
-    private static final String EMAIL = "darkmank79@gmail.com";
-    private static final String PASSWORD = "lg8R6gl4gjml/2yvEqLr";
-    private static final String BASE_URL = "https://darkmank97.testrail.io/";
+    private static final String EMAIL = "califvibe97@gmail.com";
+    private static final String PASSWORD = "ntqblqT3pVjVN6ipTmQA";
+    private static final String BASE_URL = "https://califvide97.testrail.io/";
 
     public static String getEMAIL() {
         return EMAIL;
